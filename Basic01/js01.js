908c9038103dc588e60acefec14fa66b5856f80a
angela <peekaboo32@naver.com>
2023-04-18 09:23:14 +0900

    자바스크립트 기초 공부

---
+++ Basic01/js01.js
@@ -0,0 +1,14 @@
+const ironMan = {
+    name: '토니 스타크',
+    actor: '로버트 다우니 주니어',
+    alias: '아이언맨'
+  };
+  
+  const captainAmerica = {
+    name: '스티븐 로저스',
+    actor: '크리스 에반스',
+    alias: '캡틴 아메리카'
+  };
+  
+  console.log(ironMan);
+  console.log(captainAmerica);
