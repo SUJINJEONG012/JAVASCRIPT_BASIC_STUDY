7433c4f57be4e7fcb26d3806bc8af966ce3dc731
angela <peekaboo32@naver.com>
2023-04-18 15:27:35 +0900

    마우스 조작으로 mousedown, mouseup 이벤트 리스너 함수

---
+++ 마우스조작이벤트/index.js
@@ -0,0 +1,15 @@
+//조작영역
+const logArea = document.querySelector('#log2');
+
+//대상영역에서 마우스 버튼을 누르면 로그를 출력 
+logArea.addEventListener('mousedown', ()=>{
+    logArea.innerHTML= `마우스 클릭 이벤트 발생`;
+});
+
+logArea.addEventListener('mouseup', ()=>{
+    logArea.innerHTML = `마우스 버튼을 떼는 이벤트 발생`;
+});
+
+logArea.addEventListener('mousemove', ()=>{
+    logArea.innerHTML = `마우스 이동 이벤트 발생`;
+});
