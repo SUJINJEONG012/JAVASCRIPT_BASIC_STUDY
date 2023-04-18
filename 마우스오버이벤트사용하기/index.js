5ab8e0ae0f400b23851240f0bc43bc52886ecc78
angela <peekaboo32@naver.com>
2023-04-18 17:25:40 +0900

    자바스크립트 공부

---
+++ 마우스오버이벤트사용하기/index.js
@@ -0,0 +1,12 @@
+document.querySelector('.box').addEventListener("mouseover", ()=>{
+console.log('box 요소 위에 마우스가 있음');
+});
+
+
+document.querySelector('.inner').addEventListener("mouseover", ()=>{
+    console.log('inner 요소위치에 마우스가 잇음');
+});
+    
+function log(message){
+
+}
