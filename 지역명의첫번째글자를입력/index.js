b3d928906aba552f3640c2115b0f3ca2619dc3de
angela <peekaboo32@naver.com>
2023-04-18 14:45:04 +0900

    지역명 스크립트

--- 지역명의첫번째글자를입력/index.js
+++ 지역명의첫번째글자를입력/index.js
@@ -4,6 +4,8 @@ const searchWordText = document.querySelector("#search-word-input");
 /* 지역리스트 */
 const prefectureList = document.querySelectorAll('#prefecture-list button');
 
+
+
 //문자가 입력될때마다 데이터 체크 작업실행
 searchWordText.addEventListener("keyup", ()=>{
     //입력한 검색어
@@ -11,20 +13,21 @@ searchWordText.addEventListener("keyup", ()=>{
 
     //지역리스트의 루프처리
     prefectureList.forEach((element) => {
-        //검색어가 없으면 모두 표시
+        //검색어가 없거나 공백이면   모두 표시
         if(!searchWord || searchWord === ''){
-            element.classList.remove('hide');
+            element.classList.remove('none');
             return;
         }
+        
 
         //데이터 name가져오기
         const prefectureName = element.dataset.name;
         const phonetic = element.dataset.phonetic;
 
         if(searchWord.charAt(0)=== prefectureName.charAt(0)|| searchWord.charAt(0) === phonetic.charAt(0)){
-            element.classList.remove('hide');
+            element.classList.remove('none');
         }else{
-            element.classList.add('hide');
+            element.classList.add('none');
         }
     
     });
