65ff6e16ad0e1391f97181c681897a4311f898d7
angela <peekaboo32@naver.com>
2023-04-19 13:12:25 +0900

    부모노드.children dom에 있는 요소 들고오기

---
+++ 부모자식전후요소읽어오기/index.js
@@ -0,0 +1,7 @@
+const parentElement = document.querySelector('#parent');
+console.log(parentElement.children);
+
+const firstElementChild = parentElement.firstElementChild;
+console.log(firstElementChild);
+console.log(firstElementChild.nextElementSibling);
+console.log(firstElementChild.parentNode);
