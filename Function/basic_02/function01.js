f7be14c62d54356858dedd25e773f5bee83418dd
angela <peekaboo32@naver.com>
2023-07-19 14:29:27 +0900

    객체는 값이 같다고해서 treu아님, 주소가 달라서, 원시값은 값이 같으면 true

--- Function/basic_02/function01.js
+++ Function/basic_02/function01.js
@@ -25,3 +25,55 @@ const angela = {
 이름이 필요하다 > 객체 리터럴
 이름이 필요없다 > 배열
 */
+
+console.log(angela.name);
+console.log(angela['name']);
+
+// 객체 수정 , 속성에 접근해서 수정 
+angela.gender='F';
+console.log(angela);
+
+//객체 속성 제거
+delete angela.gender;
+
+
+// 객체의 속성 값으로 함수를 넣었을때 이 속성을 특별히 '메서드' 라 한다.
+
+//debug 객체안에 test메서드
+const debug= {
+    test :  function (value){
+        console.log(value);
+    },
+}
+debug.test('Hello, World');
+
+/*
+객체간의 비교 , 객체는 서로 비교하면 false가 나옴. 배열도 마찬가지, 함수도 마찬가지
+객체끼리 비교해서 true로 나오게 할려면, 변수안에 저장을 해놓고 비교를 해야한다.
+*/
+
+const a = {name: "angela"};
+const array = [1,2,a];
+console.log(a === array[2]);
+
+// 객체가 아닌 값은 원시값
+
+//참조할땐 true
+const a2 = {name: 'angela'}
+const b = a2;
+
+a2.name ='Sujun';
+b.name = 'Sujin';
+
+
+const angela = {
+    name : {
+        first:'정',
+        last:'수진',
+    },
+    gender : 'w',
+};
+
+angela.name.last;
+angela['name']['last'];
+
